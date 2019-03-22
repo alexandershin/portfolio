9d0c378c5728d946d378ccd67c40ff35fb999ce1
Alexander <alexander.shin87@gmail.com>
2019-03-22 01:21:23 -0400

    mobile and other styling

--- scripts/script.js
+++ scripts/script.js
@@ -14,16 +14,18 @@ $(function () {
 
   // // CLOSE AFTER CLICKING OUTSIDE THE NAV (test play around)
 
-  $("#burger").click(function () {
-    $(this).find(".header-nav").toggleClass(".show");
-  });
-});
-$(document).on("click", function (event) {
-  const $menu = $(".header-nav");
-  if ($menu !== event.target && !$menu.has(event.target).length) {
-    $(".header-nav").toggleClass(".show");
-  }      
+//   $("#burger").click(function () {
+//     $(this).find(".header-nav").toggleClass(".show");
+//   });
+// });
+// $(document).on("click", function (event) {
+//   const $menu = $(".header-nav");
+//   if ($menu !== event.target && !$menu.has(event.target).length) {
+//     $(".header-nav").toggleClass(".show");
+//   }      
 
+
+// -------
 
   // // CLOSE AFTER CLICKING OUTSIDE THE NAV
   // const $menu = $('.header-nav');
@@ -74,12 +76,41 @@ $(document).on("click", function (event) {
   // });
 
 
+  // AOS 
+  AOS.init();
+
+  // You can also pass an optional settings object
+  // below listed default settings
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+  });
+
+
 
   // DESIGNED BY https://codepen.io/bob6664569/pen/rOzmve
   particlesJS('particles-js', {
     particles: {
       number: {
-        value: 3000,
+        value: 2000,
         // value: 800,
         density: {
           enable: true,
